e75e67e92c3701bb8be4c2b216d32c253634b877
w453y <awasey8905@gmail.com>
2023-12-22 14:18:54 +0530

    fixed bug

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import replicate
 import os
+import toml
 import sqlite3
+
+# Function to load API token from .streamlit/secrets.toml
+def load_api_token():
+    secrets_path = os.path.join(os.getcwd(), ".streamlit", "secrets.toml")
+    if os.path.exists(secrets_path):
+        try:
+            secrets = toml.load(secrets_path)
+            return secrets.get("secrets", {}).get("REPLICATE_API_TOKEN", "")
+        except Exception as e:
+            st.warning(f"Error loading API token from secrets.toml: {e}")
+    return ""
 
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
@@ -22,27 +34,33 @@ conn.commit()
 # Replicate Credentials
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot')
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+    # Load API token from .streamlit/secrets.toml
+    replicate_api = load_api_token()
+
+    if replicate_api:
+        st.success('API key loaded from secrets.toml!', icon='✅')
+    else:
+        st.warning('API key not found in secrets.toml. Please enter your credentials!', icon='⚠️')
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+
+    # Set the model to 'Llama2-13B' (fixed value)
+    selected_model = 'Llama2-13B'
+    llm = "meta/llama-2-13b-chat:f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d"
+
+    # Set fixed values for temperature, top-p, and max length
+    temperature = 0.75
+    top_p = 1.0
+    max_length = 800
+
+    # Set values for additional parameters
+    debug = False
+    top_k = 50
+    system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
+    max_new_tokens = 500
+    min_new_tokens = -1
+
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -53,7 +71,12 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
 def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
@@ -61,10 +84,19 @@ def generate_llama2_response(prompt_input):
             string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run(
-        'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
-        input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-               "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
+    output = replicate.run(llm, 
+                           input={
+                               "prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                               "temperature": temperature,
+                               "top_p": top_p,
+                               "max_length": max_length,
+                               "repetition_penalty": 1,
+                               "debug": debug,
+                               "top_k": top_k,
+                               "system_prompt": system_prompt,
+                               "max_new_tokens": max_new_tokens,
+                               "min_new_tokens": min_new_tokens
+                           })
     return output
 
 # Save user input in the database
@@ -74,34 +106,25 @@ def save_user_input(user_input):
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
     # Save user input to the database
     save_user_input(prompt)
-
-    # Display user input in the chat
-    st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
 
-    # Generate a new response if the last message is not from the assistant
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = generate_llama2_response(prompt)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
-                    placeholder.markdown(full_response)
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
                 placeholder.markdown(full_response)
-        message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
-
-# Button to clear chat history
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
 
 # Close the database connection
 conn.close()
-
